1d84a1166641ff92d8f02fce0659511f97288d45
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-05-21 18:24:09 +0500

    not working
    continue

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ function addtask(){
     
 }
 
+function clear(){
+    const inputbox = document.getElementById("input");
+    inputbox.value = "";
+}
 
 // let li = document.createElement("li");
 //         li.innerHTML = inputbox.value;
